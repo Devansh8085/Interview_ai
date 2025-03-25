aa9b52f9aa4512d4937cc1e4e9f1d29ec30f34d8
Devansh Kumar Mishra <116156004+Devansh8085@users.noreply.github.com>
2025-03-25 10:43:47 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://neondb_owner:npg_v2XH5DSuEUkp@ep-autumn-recipe-a5ga8kz3-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+        url:xxx,
     }
 };
